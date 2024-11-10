abcfba05b55f2e453baaacd561cd5e3fb8d94f39
Preetha8 <preethamadkuru02@gmail.com>
2024-11-10 12:51:41 +0530

    Update App.java

--- src/main/java/com/facebook/App.java
+++ src/main/java/com/facebook/App.java
@@ -5,6 +5,8 @@ package com.facebook;
  */
 public class App {
     public static void main(String[] args) {
+        int a=10;
         System.out.println("Hello World!");
+          System.out.println(a);
     }
 }
